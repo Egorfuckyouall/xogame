9c9b75d35a3062a5484d35ca8706f4c6770a3eff
Egorfuckyouall <wowiphone@icloud.com>
2017-09-23 11:34:11 +0800

    Create xogame.hpp

---
+++ xogame.hpp
@@ -0,0 +1,17 @@
+#ifndef timhuangxogame_hpp
+#define timhuangxogame_hpp
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#endif /* timhuangxogame_hpp */
+using namespace std;
+
+////////////////////可以記錄第i步走在哪///////////////////////////////////////////
+class recording{
+public:
+    int move[10] = {0,0,0,0,0,0,0,0,0,0};
+};
+
+
+char x = 'x'; char o ='o';
+
