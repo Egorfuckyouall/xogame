ab856a06e2553273e3df935f7555add4818f526e
Beckroach <wowiphone@icloud.com>
2017-10-01 10:10:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+//
+//  main.cpp
+//  xogame
+//
+//  Created by 呈澤 林 on 2017/8/6.
+//  Copyright © 2017年 呈澤 林. All rights reserved.
+//
+
+#include<iostream>
+#include<cstring>
+#include"timhuangxogame.hpp"
+using namespace std;
+
+
 int main(){
     cout << "開始執行程式"<<endl;;
     tableview A;
@@ -13,17 +27,14 @@ int main(){
                 tableview B;
                 B = A;
                 Bv.virtalmove(B);
-                cout << Bv.choose(A) << "________________999999999999999" << endl;
                 A.move(A,Bv.choose(A));
             }
         }
         else if (A.stepcout%2 == 1){
-            A.show();
-            cout << "換你拉" << endl;
+            cout << "電腦下完後棋盤結果如上，換你拉" << endl;
             int playermove;
             cin >> playermove;
             A.move(A, playermove);
-            A.show();
         }
     }
 }
